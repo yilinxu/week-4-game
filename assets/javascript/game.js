27aff3459f5e209f2bd1efffb502f5a6a1d4d1c1
yilinxu <yellen10@gmail.com>
2017-03-07 15:02:38 -0600

    change js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -131,6 +131,7 @@ $(".result").on("click", "button", function(){
     $('.your_character').html("<p class = 'show'>" + "Your Character" + "</p>"
         +"<br><br><br><br><br><br>");
     $(".result").html("<div class = 'clear'></div>");
+    $("..available_enimies".html(""));
     defeated_enemy = 0;
     attack_number = 0;
 });
